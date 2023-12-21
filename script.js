372c2d619299a88618e9d5220b3ca13e21609d44
re-d-e-v <resul_elezi@protonmail.com>
2023-12-21 20:29:42 +0100

    #

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const EMAIL = document.getElementById("email");
+const BUTTON = document.getElementById("button");
+const FORM = document.getElementById("form");
+const ERROR_MSG = document.querySelector(".email-msg");
+
+
+BUTTON.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const REG_EX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(EMAIL.value === "") {
+        FORM.classList.add("error");
+        ERROR_MSG.innerHTML = "Email is required";
+    } else if(!EMAIL.value.match(REG_EX)) {
+        FORM.classList.add("error");
+        ERROR_MSG.innerHTML = "Valid email required";
+        EMAIL.style.color = "#ff6257"
+    } else {
+        FORM.classList.remove("error");
+        EMAIL.style.color = "";
+    }
+});
